a4089a3967d445291bddaae39f66de6a5782398c
Angular CLI <angular-cli@angular.io>
2017-09-15 13:51:48 -0400

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { IEEElectivePickerPage } from './app.po';
+
+describe('iee-elective-picker App', () => {
+  let page: IEEElectivePickerPage;
+
+  beforeEach(() => {
+    page = new IEEElectivePickerPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
