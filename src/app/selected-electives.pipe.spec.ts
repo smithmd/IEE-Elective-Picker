3e26ad45bea8e805a21a2daad47437237965ba29
Michael Smith <smithmd@interlochen.org>
2017-10-12 16:24:42 -0400

    adding pipe to filter electives and some display changes

---
+++ src/app/selected-electives.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SelectedElectivesPipe } from './selected-electives.pipe';
+
+describe('SelectedElectivesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SelectedElectivesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
