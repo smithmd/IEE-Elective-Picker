88fcd87b2cb5811e9d36a3316508dd9ee0b1f245
Michael Smith <smithmd@interlochen.org>
2017-10-13 11:19:45 -0400

    handle empty lists of electives on review page

--- src/app/review-container/review-container.component.ts
+++ src/app/review-container/review-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ElectiveDataService} from '../elective-data-service';
 import {Education} from '../classes/education';
 
@@ -9,14 +9,26 @@ import {Education} from '../classes/education';
 })
 export class ReviewContainerComponent implements OnInit {
   private education: Education;
-  private complete: boolean = false;
+  private readyToSubmit: boolean = false;
+  private hasPrimaryElectivesByPMId: { [pm_id: string]: boolean } = {};
+  private hasAlternateElectivesByPMId: { [pm_id: string]: boolean } = {};
 
-  constructor(private electiveDataService: ElectiveDataService) { }
+  constructor(private electiveDataService: ElectiveDataService) {
+  }
 
   ngOnInit() {
     this.electiveDataService.education.subscribe({
       next: ed => {
         this.education = ed;
+        ed.programMajorIds.forEach(pmId => {
+          this.hasPrimaryElectivesByPMId[pmId] = ed.electivesByProgramMajorIds[pmId].reduce((value, e) => {
+            return value || e.isPrimary;
+          }, false);
+
+          this.hasAlternateElectivesByPMId[pmId] = ed.electivesByProgramMajorIds[pmId].reduce((value, e) => {
+            return value || e.isAlternate;
+          }, false);
+        });
       }
     });
   }
