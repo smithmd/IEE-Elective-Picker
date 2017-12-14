b16a3633daf243405441688cab3642bb308ca872
Mike Smith <smithmd@interlochen.org>
2017-12-14 10:18:03 -0500

    fixed a bad link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -71,7 +71,7 @@ export class AppComponent implements OnInit {
   }
 
   get privateLessonInstructions(): string {
-    return '<p><a href="\' + this.privateLessonFormLink + \'" target="_blank">Please complete this form</a> if you are ' +
+    return '<p><a href="' + this.privateLessonFormLink + '" target="_blank">Please complete this form</a> if you are ' +
       'interested in requesting an elective private lesson. An elective ' +
       'private lesson will take the place of one of the elective selections made on this page.</p>' +
       '<p>In order to be eligible for elective private lessons, you must have at lest three years of experience ' +
