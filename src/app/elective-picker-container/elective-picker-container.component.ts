37ae11e05d6e734a1eb548ee7aef8ee095b264ef
Mike Smith <smithmd@interlochen.org>
2018-01-23 14:05:12 -0500

    filters now reset if primary/alternate switches

--- src/app/elective-picker-container/elective-picker-container.component.ts
+++ src/app/elective-picker-container/elective-picker-container.component.ts
@@ -1,5 +1,7 @@
 import {Component, DoCheck, Input, OnInit} from '@angular/core';
 import {Elective} from '../classes/elective';
+import {ElectiveDataService} from '../services/elective-data-service';
+import {FilterListItem} from '../classes/filter-list-item';
 
 @Component({
   selector: 'iee-elective-picker-container',
@@ -11,8 +13,9 @@ export class ElectivePickerContainerComponent implements OnInit, DoCheck {
   private _oldTabIndex = 0;
   @Input() electives: Elective[];
   @Input() tabIndex: number;
+  private typeFilters: FilterListItem[] = [];
 
-  constructor() {
+  constructor(private electiveDataService: ElectiveDataService) {
   }
 
   ngDoCheck(): void {
@@ -23,9 +26,20 @@ export class ElectivePickerContainerComponent implements OnInit, DoCheck {
   }
 
   ngOnInit() {
+    this.electiveDataService.typeFilterList.asObservable().subscribe({
+      next: filters => {
+        this.typeFilters = filters;
+      }
+    })
   }
 
   onClickTypeOption(type: string) {
+    // only reset the list of filters if the prim/alt actually changes
+    if (this.electiveOptionsType !== type) {
+      this.typeFilters.forEach(f => {
+        f.isSelected = false;
+      });
+    }
     this.electiveOptionsType = type;
   }
 
